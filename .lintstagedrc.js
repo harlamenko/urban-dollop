302001aa5adb048e03f51015f32d3c256c6d2477
a <>
2024-05-29 20:00:14 +0300

    fix: linting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "src/**/*.ts, projects/**/*.ts": ["ng-lint-staged lint --"],
+    "src/**/*.ts, projects/**/*.ts": ["npm run lint:ts"],
     "src/**/*.scss": ["npm run lint:styles"]
 };
