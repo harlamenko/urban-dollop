2967bb02a8689217e9c43392de777f16dae71b96
a <>
2024-05-30 10:30:02 +0300

    Revert "feat: BUG"
    This reverts commit 762d8586e0d51db60f36ef60ec42131d9dba5179.

--- projects/evo-ui-kit/src/lib/evo-ui-kit.module.ts
+++ projects/evo-ui-kit/src/lib/evo-ui-kit.module.ts
@@ -12,7 +12,7 @@ import {EvoIsExpandedDirective, EvoLetDirective} from './directives';
 
 export { WINDOW_PROVIDERS };
 
-const directive = [
+const directives = [
     EvoUiClassDirective,
     EvoClickOutsideDirective,
     EvoIsExpandedDirective,
